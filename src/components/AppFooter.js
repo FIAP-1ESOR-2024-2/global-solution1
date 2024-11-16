8d1067bc65d28334d260e16e4f85bb874e5e2b37
Enrico Brosque Palma <enricob.palma@gmail.com>
2024-11-16 17:15:49 -0300

    Changes style in footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,12 +4,12 @@ class AppFooter extends HTMLElement {
     this.innerHTML = `
       <div class="mt-auto">
         <div class="p-5 component-footer d-flex flex-row justify-content-between">
-          <div class="flex-fill">
+          <div class="flex-fill d-flex flex-column align-items-center">
             <span class="fs-4 fw-bold">
               Placeholder
             </span>
           </div>
-          <div class="flex-fill">
+          <div class="flex-fill d-flex flex-column align-items-center">
             <span class="fs-4 fw-bold">
               Links rápidos
             </span>
@@ -31,10 +31,18 @@ class AppFooter extends HTMLElement {
               </li>
             </ul>
           </div>
-          <div class="flex-fill">
+          <div class="flex-fill d-flex flex-column align-items-center">
             <span class="fs-4 fw-bold">
               Parceiros
             </span>
+            <div class="d-flex flex-column">
+              <img src="./assets/images/White/FIAP.png" alt="Logo do parceiro FIAP" class="partner-logo">
+              <img src="./assets/images/White/SAP.png" alt="Logo do parceiro SAP" class="partner-logo">
+              <img src="./assets/images/White/FIA.png" alt="Logo do parceiro FIA" class="partner-logo">
+              <img src="./assets/images/White/Mahindra.png" alt="Logo do parceiro Mahindra" class="partner-logo">
+              <img src="./assets/images/White/Ultragaz.png" alt="Logo do parceiro Ultragaz" class="partner-logo">
+              <img src="./assets/images/White/Ultracargo.png" alt="Logo do parceiro Ultracargo" class="partner-logo">
+            </div>
           </div>
         </div>
         <div class="rights py-3 text-center">
